ee70633e6c5d90f2344e4280d6281a6f5238ae77
Divyam Singh <divyamsingh1918@gmail.com>
2025-03-06 22:35:12 +0530

    updates

--- server/server.ts
+++ server/server.ts
@@ -1,8 +1,10 @@
 import express from "express"
 import { Request,Response } from "express";
+import cors from "cors"
 const app = express();
 const port = 4000;
 app.use(express.json());
+app.use(cors())
 
 app.get('/',(req:Request,res:Response)=>{
   res.json({"message":"It works"})
