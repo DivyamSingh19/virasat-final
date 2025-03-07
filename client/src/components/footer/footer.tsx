336d456711ed3cb6bc75b13fc4c4db2b1ef4bc8d
Aditya Sharma <adisharma4857@gmail.com>
2025-03-07 11:10:25 +0530

    initial commit

---
+++ client/src/components/footer/footer.tsx
@@ -0,0 +1,5 @@
+export default function Footer() {
+    return (
+        <footer className="bg-neutral-100">This is my footer</footer>
+    )
+}
---
+++ client/src/components/header/header.tsx
@@ -0,0 +1,5 @@
+export default function Header() {
+    return (
+        <header className="bg-neutral-100">This is my header</header>
+    )
+}
